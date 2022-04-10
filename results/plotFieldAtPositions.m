4aeadc50a68d0b6168d092968d84c11250832979
Piotr-Pietruszka <piopie98@wp.pl>
2022-04-10 19:07:15 +0200

    add dt to plot

--- results/plotFieldAtPositions.m
+++ results/plotFieldAtPositions.m
@@ -7,11 +7,11 @@ Nz=spatial_temporal_dimensions(1);
 Nt=spatial_temporal_dimensions(2);
 field = fread(fptr, [Nz, Nt],'double');
 
-
+dt=3.302285e-017;
 for i = 1:length(z_points)
     z = z_points(i);
     field_at_z = field(z, 1:end);
-    plot(field_at_z);
+    plot((1:Nt).*dt, field_at_z);
     title(strcat('E field amplitude at cell: ', num2str(z)));
     xlabel('t (steps)');
     

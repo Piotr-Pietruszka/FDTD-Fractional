9f2f0001349fb242157da655f661babb8f2dd74f
Piotr-Pietruszka <piopie98@wp.pl>
2022-04-11 21:41:11 +0200

    measure sim time

--- main.c
+++ main.c
@@ -9,9 +9,9 @@ int main()
     double dt = 0.1*0.99*dz/C_CONST;
     // double dt = 1e-18;
 
-    double Lz = 250.0e-6;
+    double Lz = 150.0e-6;
     int Nz = (int) (Lz/dz);
-    double T = 20e-14;
+    double T = 25e-14;
     int Nt =  (int) (T/dt);
     double alpha = 0.99;
 
@@ -27,6 +27,13 @@ int main()
     double* Ex_inc = calloc(Nt, sizeof(double));
     double* Hy_inc = calloc(Nt, sizeof(double));
 
+    clock_t start_time, end_time;
+    start_time = clock();
+
     simulation(dz, Nz, dt, Nt, alpha, Ex, Hy, Ex_inc, Hy_inc);
+
+    end_time = clock();
+    double sim_time = ((double)(end_time - start_time)) / CLOCKS_PER_SEC;
+    printf("simulation time: %lf\n", sim_time);
 
 }
